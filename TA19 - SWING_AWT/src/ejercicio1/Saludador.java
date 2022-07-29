ad1fac0da9ca4bd0ca343ed23382d7eb42456489
IzanDuesoLopez <izandueso@gmail.com>
2022-07-29 09:41:34 +0200

    Ejercicio Hecho Entre Todos

--- TA19 - SWING_AWT/src/ejercicio1/Saludador.java
+++ TA19 - SWING_AWT/src/ejercicio1/Saludador.java
@@ -1,32 +1,71 @@
 package ejercicio1;
 
-import java.awt.TextField;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
-public class Saludador extends JFrame{
-	
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class Saludador extends JFrame {
+
 	private JPanel contentPane;
-	
-	public Saludador() {
-		
-		setTitle("Ventana Saludador"); // Titulo de la ventana
-		setBounds(500,200,300,350); // Dimensiones de la ventana
-		setDefaultCloseOperation(EXIT_ON_CLOSE); // Cuando cerremos la ventana se acaba la aplicacion
-		setVisible(true); // Hacemos visible la ventana
-		
-		contentPane = new JPanel(); // Creamos el panel principal
-		contentPane.setLayout(null); // Indicamos su diseño
-		
-		setContentPane(contentPane); // Asignamos el panel a la ventana
-		
-		JLabel etiqueta = new JLabel("Saluditos");
-		etiqueta.setBounds(60, 20, 100, 200);
-		contentPane.add(etiqueta);
-		
-		JTextField textField = new JTextField();
-		textField.setBounds(50,150,90,30);
-		contentPane.add(textField);
+	private JTextField nombreUser;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Saludador frame = new Saludador();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-	
+
+	/**
+	 * Create the frame.
+	 */
+	public Saludador() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		nombreUser = new JTextField();
+		nombreUser.setBounds(71, 120, 290, 29);
+		contentPane.add(nombreUser);
+		nombreUser.setColumns(10);
+		
+		JButton btnNewButton = new JButton("¡Salúdame!");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(btnNewButton, "Hola " + nombreUser.getText());
+			}
+		});
+		btnNewButton.setBounds(169, 168, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("¡Escribe un nombre para saludar!");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(53, 47, 330, 62);
+		contentPane.add(lblNewLabel);
+	}
 }

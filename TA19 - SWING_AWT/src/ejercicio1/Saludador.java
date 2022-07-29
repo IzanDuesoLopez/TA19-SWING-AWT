bf09d244a91f83cad2e847cfd4cc4b2a4aa9a5f6
IzanDuesoLopez <izandueso@gmail.com>
2022-07-29 09:11:14 +0200

    Pruebas

--- TA19 - SWING_AWT/src/ejercicio1/Saludador.java
+++ TA19 - SWING_AWT/src/ejercicio1/Saludador.java
@@ -1,5 +1,23 @@
 package ejercicio1;
 
-public class Saludador {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class Saludador extends JFrame{
+	
+	private JPanel contentPane;
+	
+	public Saludador() {
+		
+		setTitle("Ventana Saludador"); // Titulo de la ventana
+		setBounds(500,200,300,350); // Dimensiones de la ventana
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Cuando cerremos la ventana se acaba la aplicacion
+		setVisible(true); // Hacemos visible la ventana
+		
+		contentPane = new JPanel(); // Creamos el panel principal
+		contentPane.setLayout(null); // Indicamos su diseño
+		
+		
+	}
+	
 }
--- TA19 - SWING_AWT/src/ejercicio1/SaludadorMainApp.java
+++ TA19 - SWING_AWT/src/ejercicio1/SaludadorMainApp.java
@@ -3,7 +3,8 @@ package ejercicio1;
 public class SaludadorMainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
 
 	}
 
